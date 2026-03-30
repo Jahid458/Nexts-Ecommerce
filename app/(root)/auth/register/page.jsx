f0b441b00d3a790dfdd4969c3d17a80cffcb89a2
Jahid458 <Jahidul898@outlook.com>
2026-03-30 18:35:40 +0600

    register page ui design

--- app/(root)/auth/register/page.jsx
+++ app/(root)/auth/register/page.jsx
@@ -19,30 +19,37 @@ import ButtonLoading from "../../../../components/Application/ButtonLoading";
 import { z } from "zod";
 import { FaRegEyeSlash } from "react-icons/fa";
 import { FaRegEye } from "react-icons/fa6";
-import Link from "next/link";
-import { WEBSITE_REGISTER } from "@/routes/WebsiteRoute";
+import Link from "next/link"
+import { WEBSITE_LOGIN } from "@/routes/WebsiteRoute";
 
-const LoginPage = () => {
+
+
+const RegisterPage = () => {
   const [loading, setLoading] = useState(false);
   const [isTypePassword, setIsTypePassword] = useState(true);
 
-  const formSchema = zSchema
-    .pick({
-      email: true,
+  const formSchema = zSchema.pick({
+     name:true, email: true, password: true 
+    }).extend({
+        confirmPassword:z.string()
+    }).refine((data) => data.password === data.confirmPassword,{
+        message: 'Password and confirm password must be same',
+        path: ['confirmPassword']
     })
-    .extend({
-      password: z.string().min("3", "password fiels is required"),
-    });
+
+
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      name: "",
       email: "",
       password: "",
+      confirmPassword: ''
     },
   });
 
-  const handleLogicSubmit = async (values) => {
+  const handleRegisterSubmit = async (values) => {
     console.log(values);
   };
 
@@ -60,13 +67,32 @@ const LoginPage = () => {
             />
           </div>
           <div className="text-center">
-            <h1 className=" text-3xl  font-bold">Login Into Account</h1>
-            <p>Login into Your Account by filling out the from below</p>
+            <h1 className=" text-3xl  font-bold">Create  Account</h1>
+            <p>Create New Account by filling out the from below</p>
           </div>
 
           <div>
             <Form {...form}>
-              <form onSubmit={form.handleSubmit(handleLogicSubmit)}>
+              <form onSubmit={form.handleSubmit(handleRegisterSubmit)}>
+                <div className="mb-3">
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Full Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="text"
+                            placeholder="Enter Your Name"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
                 <div className="mb-3">
                   <FormField
                     control={form.control}
@@ -86,13 +112,34 @@ const LoginPage = () => {
                     )}
                   />
                 </div>
-                <div className="mb-5">
+
+                                <div className="mb-5">
                   <FormField
                     control={form.control}
                     name="password"
                     render={({ field }) => (
                       <FormItem className="relative">
                         <FormLabel>password</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="**********"
+                            {...field}
+                          />
+                        </FormControl>
+                     
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="mb-5">
+                  <FormField
+                    control={form.control}
+                    name="cofirmPassword"
+                    render={({ field }) => (
+                      <FormItem className="relative">
+                        <FormLabel>Confirm password</FormLabel>
                         <FormControl>
                           <Input
                             type={isTypePassword ? "password" : "text"}
@@ -112,24 +159,23 @@ const LoginPage = () => {
                     )}
                   />
                 </div>
+
                 <div className="mb-3">
                   <ButtonLoading
                     loading={loading}
                     type="Submit"
-                    text="Login"
+                    text="Create Account"
                     className="w-full cursor-pointer"
                   />
                 </div>
 
                 <div className="text-center">
                   <div className="flex justify-center items-center gap-1">
-                     <p>Don't have Account? </p>
-                     <Link href={WEBSITE_REGISTER} className="text-primary underline">Create Account</Link>
+                     <p>Already have an  Account? </p>
+                     <Link href={WEBSITE_LOGIN} className="text-primary underline">Login!</Link>
                   </div>
 
-                  <div className="mt-3">
-                     <Link href='' className="text-primary underline">Forgot Password?</Link>
-                  </div>
+             
                   
                 </div>
                   
@@ -141,4 +187,5 @@ const LoginPage = () => {
     </div>
   );
 };
-export default LoginPage;
+
+export default RegisterPage;
